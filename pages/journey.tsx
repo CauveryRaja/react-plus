b143ad04340b2416b6252bb498d2322ae6ec9893
CauveryRaja <cauveryraja25@gmail.com>
2024-02-29 09:04:33 +0530

    Cleanup

---
+++ pages/journey.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Journey() {
+  return (
+    <>
+      <Head>
+        <title>Form journey</title>
+      </Head>
+      <main></main>
+    </>
+  );
+}
