df3acd896b61f82a7210b36996f4c82142fbcf4e
CauveryRaja <cauveryraja25@gmail.com>
2024-03-13 09:04:04 +0530

    Install React query to fetch category data

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # React Plus
+
 A playground for all features implemented using React and Typescript. This project is bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Tech stacks used
+
 - [React](https://react.dev/) : User Interface Library
 - [React Testing Library](https://testing-library.com/) : User-centric UI testing library
 - [Prettier](https://prettier.io/) : Code formatter
 - [Husky](https://typicode.github.io/husky/#/) : Git Hooks for commit and push actions
 - [Lint staged](https://github.com/okonet/lint-staged) : Runs linters against git staged files
+- [Tanstack Query](https://tanstack.com/query/latest/docs/framework/react/overview) : FKA React Query - data fetching library
 
 ## Assumptions
+
 - Only use npm. Do not use yarn.
 - Node version : Check the `.nvmrc` file
 
 ## Getting started
+
 Start the development server
+
 ```
 npm i
 npm dev
--- features/journeyForms/providers/CategoryDataProvider.tsx
+++ features/journeyForms/providers/CategoryDataProvider.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren, createContext } from "react";
 import { SUPPORTED_CATEGORIES } from "../constants";
+import { useQuery } from "@tanstack/react-query";
 
 type CategoryProviderProps = {
   category?: string;
@@ -11,13 +12,28 @@ const CategoryDataProvider = ({
   category,
   children,
 }: PropsWithChildren<CategoryProviderProps>) => {
-  if (!(category && SUPPORTED_CATEGORIES.includes(category as CategoryId)))
+  const isCategorySupported = !!(
+    category && SUPPORTED_CATEGORIES.includes(category as CategoryId)
+  );
+
+  const categoryQuery = useQuery({
+    queryKey: ["category"],
+    queryFn: () => fetch(`/category?id=${category}`).then((res) => res.json()),
+    enabled: isCategorySupported,
+  });
+  const { data, status, error } = categoryQuery;
+
+  if (!isCategorySupported)
     return <div>Category {category} is not supported!</div>;
 
-  return (
+  if (error) return <div>Cannot fetch category details</div>;
+
+  return status === "success" ? (
     <CategoryDataContext.Provider value={{}}>
       {children}
     </CategoryDataContext.Provider>
+  ) : (
+    <div>Loading...</div>
   );
 };
 
